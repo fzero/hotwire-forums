5c6bcc3c32e6780c21e8f8e843c3bdea205ab62a
Fabio Neves <fneves@wrapbook.com>
2022-05-16 11:56:12 -0400

    Unleash the Rubocop

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,54 +1,53 @@
+# frozen_string_literal: true
+
 class DiscussionsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_discussion, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_discussion, only: %i[edit update destroy]
 
-    def index
-        @discussions = Discussion.all
+  def index
+    @discussions = Discussion.all
+  end
+
+  def new
+    @discussion = Discussion.new
+  end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+
+    respond_to do |format|
+      if @discussion.save
+        format.html { redirect_to discussions_path, notice: 'Discussion created' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def new 
-        @discussion = Discussion.new
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to discussions_path, notice: 'Discussion updated' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def create
-        @discussion = Discussion.new(discussion_params)
+  def destroy
+    @discussion.destroy!
+    redirect_to discussion_path, notice: 'Discussion removed'
+  end
 
-        respond_to do |format|
-            if @discussion.save
-                format.html { redirect_to discussions_path, notice: "Discussion created" }
-            else
-                format.html { render :new, status: :unprocessable_entity }
-            end
+  private
 
-        end
-    end
+  def discussion_params
+    params.require(:discussion).permit(:name, :closed, :pinned)
+  end
 
-    def edit
-    end
-
-    def update
-        respond_to do |format|
-            if @discussion.update(discussion_params)
-                format.html { redirect_to discussions_path, notice: "Discussion updated" }
-            else
-                format.html { render :edit, status: :unprocessable_entity }
-            end
-
-        end
-    end
-
-    def destroy
-        @discussion.destroy! 
-        redirect_to discussion_path, notice: 'Discussion removed'
-    end
-
-    private
-
-    def discussion_params
-        params.require(:discussion).permit(:name, :closed, :pinned)
-    end
-
-    def set_discussion
-        @discussion = Discussion.find(params[:id])
-    end
+  def set_discussion
+    @discussion = Discussion.find(params[:id])
+  end
 end
