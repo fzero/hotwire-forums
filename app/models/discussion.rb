2e12ea75e67d926318b06c15440497f5c6ca7344
Fabio Neves <fneves@wrapbook.com>
2022-05-17 11:58:48 -0400

    Turbo streams, actiontext and activestorage

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -5,6 +5,14 @@ class Discussion < ApplicationRecord
 
   validates :name, presence: true
 
+  after_create_commit -> { broadcast_prepend_to "discussions" }
+  after_update_commit -> { broadcast_replace_to "discussions" }
+  after_destroy_commit -> { broadcast_remove_to "discussions" }
+
+  default_scope { order(created_at: :desc) }
+  scope :pinned, -> { where(pinned: true) }
+  scope :closed, -> { where(closed: true) }
+
   def to_param
     "#{id}-#{name.first(100)}".parameterize
   end
