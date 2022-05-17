3d807241352e454ab0840292261fffa5e8ca0730
Fabio Neves <fneves@wrapbook.com>
2022-05-17 11:28:00 -0400

    Friednly links

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -4,4 +4,8 @@ class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
 
   validates :name, presence: true
+
+  def to_param
+    "#{id}-#{name.first(100)}".parameterize
+  end
 end
