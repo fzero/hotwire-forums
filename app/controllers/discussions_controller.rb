7ae2ab44b2715e144e119230c5f1a1e2daf785b2
Fabio Neves <fneves@wrapbook.com>
2022-05-18 16:34:31 -0400

    Some refactoring

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -35,37 +35,7 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
-        @discussion.broadcast_replace(
-          partial: 'discussions/header',
-          locals: { discussion: @discussion }
-        )
-
-        # This looks ugly af
-        if @discussion.saved_change_to_category_id?
-          old_category_id, new_category_id = @discussion.saved_change_to_category_id
-
-          old_category = Category.find(old_category_id)
-          new_category = Category.find(new_category_id)
-
-          # Update discussions in category lists
-          @discussion.broadcast_remove_to(old_category)
-          @discussion.broadcast_prepend_to(new_category)
-
-          # Update category list and discussion counts
-          old_category.reload.broadcast_replace_to('categories')
-          new_category.reload.broadcast_replace_to('categories')
-        end
-
-        if @discussion.saved_change_to_closed?
-          @discussion.broadcast_action_to(
-            @discussion,
-            action: :replace,
-            target: 'new_post_form', # This is the dom id of the turbo-frame
-            partial: 'discussions/posts/form',
-            locals: { post: @discussion.posts.new }
-          )
-        end
-
+        DiscussionBroadcaster.new(@discussion).broadcast!
         format.html { redirect_to @discussion, notice: 'Discussion updated' }
       else
         format.html { render :edit, status: :unprocessable_entity }
---
+++ app/models/discussion_broadcaster.rb
@@ -0,0 +1,47 @@
+class DiscussionBroadcaster
+  attr_reader(:discussion)
+
+  def initialize(discussion)
+    @discussion = discussion
+  end
+
+  def broadcast!
+    replace_header
+    replace_new_post_form if discussion.saved_change_to_closed?
+    move_categories if discussion.saved_change_to_category_id?
+  end
+
+  private
+
+  def replace_header
+    discussion.broadcast_replace(
+      partial: 'discussions/header',
+      locals: { discussion: discussion }
+    )
+  end
+
+  def replace_new_post_form
+    discussion.broadcast_action_to(
+      discussion,
+      action: :replace,
+      target: 'new_post_form', # This is the dom id of the turbo-frame
+      partial: 'discussions/posts/form',
+      locals: { post: discussion.posts.new }
+    )
+  end
+
+  def move_categories
+    old_category_id, new_category_id = discussion.saved_change_to_category_id
+
+    old_category = Category.find(old_category_id)
+    new_category = Category.find(new_category_id)
+
+    # Update discussions in category lists
+    discussion.broadcast_remove_to(old_category)
+    discussion.broadcast_prepend_to(new_category)
+
+    # Update category list and discussion counts
+    old_category.reload.broadcast_replace_to('categories')
+    new_category.reload.broadcast_replace_to('categories')
+  end
+end
