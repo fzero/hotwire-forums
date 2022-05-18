ed7b20321315ee545ad7c367fd0fd0732c5a7d9d
Fabio Neves <fneves@wrapbook.com>
2022-05-18 13:54:49 -0400

    Categories on discussions index

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,7 @@ class Category < ApplicationRecord
 
   scope :sorted, -> { order(name: :asc) }
 
+  after_create_commit -> { broadcast_prepend_to "categories" }
+  after_update_commit -> { broadcast_replace_to "categories" }
+  after_destroy_commit -> { broadcast_remove_to "categories" }
 end
