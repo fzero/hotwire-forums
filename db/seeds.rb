4a7fc3c95337c5de3b262d4e6d4852cfdbf9a736
Fabio Neves <fzero@users.noreply.github.com>
2022-05-21 12:50:51 -0400

    Proper db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Default users ---------------------------------------------------------------
+users =[
+  {
+    email: 'admin@test.com',
+    username: 'admin',
+    password: 'supersecret',
+    password_confirmation: 'supersecret',
+    admin: true
+  },
+  {
+    email: 'test@test.com',
+    username: 'test',
+    password: 'hunter2',
+    password_confirmation: 'hunter2'
+  }
+]
+User.delete_all
+User.create!(users)
+
+# Default categories ----------------------------------------------------------
+# Category model has a bunch of callbacks and we don't want
+# to trigger those right now
+categories = %w[General Shitposts SrsBsns].map do |name|
+  {
+    name: name,
+    created_at: Time.new,
+    updated_at: Time.new
+  }
+end
+Category.delete_all
+Category.insert_all!(categories)
